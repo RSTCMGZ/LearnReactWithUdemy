790aa8511b37e79ab845217d2d791da84b958b2c
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-10 23:08:59 +0300

    OnClickEvent

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -4,7 +4,13 @@ import ProductInfo from './ProductInfo'
 
 function ProductItem({ product }) {
     // const { product } = props
-
+    // const myButton = document.getElementById('myButton')
+    // myButton.addEventListener('click', function () {
+    //     console.log('sepete eklendi!');
+    // })
+    const clickHandler = () => {
+        console.log('Tıklandı');
+    }
     return (
         <div className='product-item'>
             <div className='product-image'>
@@ -14,6 +20,8 @@ function ProductItem({ product }) {
             <ProductInfo >
                 <h2>{product.productName}</h2>
                 <span>{product.productPrice}₺</span>
+                <br />
+                <button onClick={clickHandler}>Sepete Ekle</button>
             </ProductInfo>
         </div>
     )
