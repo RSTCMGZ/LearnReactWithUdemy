0dd88b7acef3b2c6af264ebf8839183bdffcec83
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-13 02:10:38 +0300

    learnprevState

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductForm.css'
 function ProductForm() {
 
+    // const [productName, setProductName] = useState('')
+    // const [productPrice, setProductPrice] = useState('')
+    // const [productImageUrl, setProductImageUrl] = useState('')
+
+
+    const [productData, setProductData] = useState(
+        {
+            productName: '',
+            productPrice: "0",
+            imageUrl: '',
+        },
+    )
     const titleChangeHandler = (event) => {
-        event.target.value
+        // setProductName(event.target.value)
+        // setProductData({
+        //     ...productData,
+        //     productName: event.target.value
+        // })
+        setProductData((prevState) => {
+            return { ...prevState, productName: event.target.value }
+        })
+    }
+    const priceChangeHandler = (event) => {
+        // setProductPrice(event.target.value)
+        // setProductData({
+        //     ...productData,
+        //     productPrice: event.target.value
+        // })
+        setProductData((prevState) => {
+            return { ...prevState, productPrice: event.target.value }
+        })
+    }
+    const imageChangeHandler = (event) => {
+        // setProductImageUrl(event.target.value)
+        // setProductData({
+        //     ...productData,
+        //     imageUrl: event.target.value
+        // })
+        setProductData((prevState) => {
+            return { ...prevState, imageUrl: event.target.value }
+        })
     }
     return (
         <form className='product-form'>
             <div className='product-form-input'>
-                <label>Ürün Adı</label>
+                <label>Ürün Adı </label>
                 <input type="text" placeholder='Ürün Adı Giriniz.' onChange={titleChangeHandler} />
             </div>
             <div className='product-form-input'>
-                <label>Ürün Fiyatı</label>
-                <input type="number" placeholder='Ürün Fiyatı Giriniz.' />
+                <label>Ürün Fiyatı </label>
+                <input type="number" placeholder='Ürün Fiyatı Giriniz.' onChange={priceChangeHandler} />
             </div>
             <div className='product-form-input'>
-                <label>Ürün Görseli</label>
-                <input type="text" placeholder='Ürün Görseli Giriniz.' />
+                <label>Ürün Görseli </label>
+                <input type="text" placeholder='Ürün Görseli Giriniz.' onChange={imageChangeHandler} />
             </div>
             <button className='product-form-button'>Ürün Ekle</button>
         </form>
