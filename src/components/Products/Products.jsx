ac283767c81d7180a16e7df7b0c4e3c04253dcd0
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-03-14 00:51:55 +0300

    learnMapFunction

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,21 +4,16 @@ import './Products.css'
 import { productData } from '../../productData'
 import NewProduct from '../NewProduct/NewProduct'
 const Products = () => {
+    // const [products, setProducts] = useState(productData)
     const [products, setProducts] = useState(productData)
-
-
-
     return (
         <div className='product-wrapper'>
             <NewProduct setProducts={setProducts} />
             <h1>Products</h1>
             <div className='products'>
-                {products.map((product) =>
-                    <ProductItem
-                        key={product.productName}
-                        product={product}
-                    />
-                )}
+                {products.map((product) => (
+                    <ProductItem key={product.productName} product={product} />
+                ))}
             </div>
         </div>
     )
